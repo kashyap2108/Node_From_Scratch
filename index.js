68642e3895883aeb71cca8f6a500850cd1a9f9f0
kashyap2108 <kashyap959821@gmail.com>
2018-08-16 10:50:31 +0530

    Checks module completed using workers model

--- index.js
+++ index.js
@@ -1,95 +1,21 @@
-//Load Dependencies
+// Dependencies
 
-const http = require("http");
-const url = require("url");
-const StringDecoder = require("string_decoder").StringDecoder;
-const config = require("./config");
-const fs = require("fs");
-const helpers = require("./lib/helpers");
-const handlers = require("./lib/handlers");
+const server = require("./lib/server");
+const workers = require("./lib/workers");
 
-// create server
+// Declare the app
+const app = {};
 
-const server = http.createServer((req, res) => {
-  // Get the URL and parse it
-  var parsedUrl = url.parse(req.url, true);
-
-  // Get the path
-  var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
-
-  // Get the query string as an object
-  var queryStringObject = parsedUrl.query;
-
-  // Get the HTTP method
-  var method = req.method;
-
-  // Get the headers as an object
-  var headers = req.headers;
-
-  // Get the payload , if any
-
-  var decoder = new StringDecoder("utf-8");
-  var buffer = "";
-  req.on("data", data => {
-    buffer += decoder.write(data);
-  });
-
-  req.on("end", () => {
-    buffer += decoder.end();
-
-    // Choose the handler;
-    var chosenHandler =
-      typeof router[trimmedPath] !== "undefined"
-        ? router[trimmedPath]
-        : handlers.notFound;
-
-    // Construct the data object to send to the handler
-
-    var data = {
-      trimmedPath: trimmedPath,
-      queryStringObject: queryStringObject,
-      method: method,
-      headers: headers,
-      payload: helpers.parseJsonToObject(buffer)
-    };
-
-    chosenHandler(data, function(statusCode, payload) {
-      statusCode = typeof statusCode == "number" ? statusCode : 200;
-      payload = typeof payload == "object" ? payload : {};
-
-      // convert the payload to a string
-
-      var payloadString = JSON.stringify(payload);
-
-      // Return the response
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(statusCode);
-      res.end(payloadString);
-      console.log("Returning this response", statusCode, payloadString);
-    });
-
-    // Route the request to the handler specified in the router
-
-    // Log the request path
-  });
-});
-
-// Listen on server
-server.listen(config.port, () => {
-  console.log(
-    "The server is listening on port " +
-      config.port +
-      " in " +
-      config.envName +
-      " node !!"
-  );
-});
-
-// Define a request router
-
-var router = {
-  ping: handlers.ping,
-  users: handlers.users,
-  tokens: handlers.tokens
+//Init function
+app.init = function() {
+  // Start the server
+  server.init();
+  // Start the workers
+  workers.init();
 };
+
+// Execute
+app.init();
+
+// Export the app
+module.exports = app;
